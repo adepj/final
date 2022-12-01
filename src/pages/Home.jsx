7d31313193086d45dddc6182313dab78bd2d45ec
Adelina Pelagio <adepj84@outlook.com>
2022-12-01 12:46:06 -0600

    final commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductsThunk } from "../store/slices/products.slice";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(getProductsThunk());
+  }, []);
+
+  console.log("log productos", products);
+
+  return (
+    <div className="container--card">
+      {products.map((product, id) => (
+        <div className="card-" key={id}>
+          <Link to={`/products/${product.id}`}>
+            <div className="container-image">
+              <img src={product.productImgs[0]} alt="" />
+            </div>
+
+            <div className="description">
+              <strong>{product.title}</strong>
+
+              <p>
+                <b>Price</b>
+              </p>
+              <span>{product.price}</span>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
